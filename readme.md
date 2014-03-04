f7e90eb03038d4aa9d79b3be09e612dd0678be2e
Micaelee <micaelee@live.com>
2014-03-04 14:42:24 -0800

    Changed name

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
-# Simple Post Types Plugin
-## By Morten Rand-Hendriksen
+# Other Post Types Plugin
+## By Micaelee Hanson
 
 This plugin was created to demonstrate the principles of using Git and GitHub and provie a simple way of adding custom post types and taxonomies to a WordPress site without adding them to the theme functions.php file and without using an advanced settings plugin.
 
--- simpleposttypes.php
+++ simpleposttypes.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name: Morten's Simple Post Types 
+ * Plugin Name: Micaelee's Other Post Types 
  * Description: A simple plugin to add custom post types and taxonomies to a WordPress site. This plugin was created to demonstrate these principles and the principles of using GitHub and is not for general use. Do not expect it to work.
  * Version: 0.0.1
- * Author: Morten Rand-Hendriksen
- * Author URI: http://mor10.com
+ * Author: Micaelee Hanson
+ * Author URI: http://mehanson.com
  * License: GPL2
  */
 
